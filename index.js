f173412d91cbcd5d42043da3348e6889618de4cf
Andrey Shaposhnikov <droniiiandruxa@gmail.com>
2023-05-26 19:53:30 +0300

    Refactored. REady for check

--- index.js
+++ index.js
@@ -1,34 +1,29 @@
-// something
 const contactsTools = require('./db/contacts');
 const yargs = require('yargs');
-
-// console.log('Sup, i am connected');
 
 const { argv } = yargs(process.argv.slice(2));
 
 const invokeAction = async ({ action, id, name, email, phone }) => {
 	switch (action) {
 		case 'list':
-			// ...
 			const allContacts = await contactsTools.getListOfContacts();
 			console.log('Here all contacts');
 			console.log(allContacts);
 			break;
 
 		case 'get':
-			// ... id
 			const getContactById = await contactsTools.getContactById(id);
+			console.log('Here contact you looking for: ');
 			console.log(getContactById);
 			break;
 
 		case 'add':
-			// ... name email phone
 			const addNewContact = await contactsTools.addContact(name, email, phone);
+			console.log('Contact was added to data base');
 			console.log(addNewContact);
 			break;
 
 		case 'remove':
-			// ... id
 			const removeContactById = await contactsTools.removeContactById(id);
 			console.log('Contact removed: ');
 			console.log(removeContactById);
