d18d7100b3e200606a6829937a669dca689e388f
Andrey Shaposhnikov <droniiiandruxa@gmail.com>
2023-05-30 11:51:11 +0300

    ADD: console.table resize

--- index.js
+++ index.js
@@ -8,25 +8,29 @@ const invokeAction = async ({ action, id, name, email, phone }) => {
 		case 'list':
 			const allContacts = await contactsTools.getListOfContacts();
 			console.log('Here all contacts');
-			console.table(allContacts);
+			console.table(allContacts, ['id', 'name', 'email', 'phone']);
+
 			break;
 
 		case 'get':
 			const getContactById = await contactsTools.getContactById(id);
 			console.log('Here contact you looking for: ');
-			console.table(getContactById);
+			console.table(getContactById, ['id', 'name', 'email', 'phone']);
+
 			break;
 
 		case 'add':
 			const addNewContact = await contactsTools.addContact(name, email, phone);
 			console.log('Contact was added to data base');
-			console.table(addNewContact);
+			console.table(addNewContact, ['id', 'name', 'email', 'phone']);
+
 			break;
 
 		case 'remove':
 			const removeContactById = await contactsTools.removeContactById(id);
 			console.log('Contact removed: ');
-			console.table(removeContactById);
+			console.table(removeContactById, ['id', 'name', 'email', 'phone']);
+
 			break;
 
 		default:
