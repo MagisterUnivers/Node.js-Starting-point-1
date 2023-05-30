83a34c7232e6592d2488cb41a37e051e0b769b81
Andrey Shaposhnikov <droniiiandruxa@gmail.com>
2023-05-30 11:58:32 +0300

    FIX: Console.table (get)

--- index.js
+++ index.js
@@ -9,28 +9,24 @@ const invokeAction = async ({ action, id, name, email, phone }) => {
 			const allContacts = await contactsTools.getListOfContacts();
 			console.log('Here all contacts');
 			console.table(allContacts, ['id', 'name', 'email', 'phone']);
-
 			break;
 
 		case 'get':
 			const getContactById = await contactsTools.getContactById(id);
 			console.log('Here contact you looking for: ');
-			console.table(getContactById, ['id', 'name', 'email', 'phone']);
-
+			console.table([getContactById], ['id', 'name', 'email', 'phone']);
 			break;
 
 		case 'add':
 			const addNewContact = await contactsTools.addContact(name, email, phone);
 			console.log('Contact was added to data base');
 			console.table(addNewContact, ['id', 'name', 'email', 'phone']);
-
 			break;
 
 		case 'remove':
 			const removeContactById = await contactsTools.removeContactById(id);
 			console.log('Contact removed: ');
 			console.table(removeContactById, ['id', 'name', 'email', 'phone']);
-
 			break;
 
 		default:
